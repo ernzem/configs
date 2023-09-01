42f5d43a71f3b2641c0a13b7502101e581a786a5
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2023-09-01 22:13:39 +0300

    nvim as vim in shell

--- shell/my_configs.bash
+++ shell/my_configs.bash
@@ -3,11 +3,12 @@
 OS="`uname`"
 case $OS in
   'Linux')
-    alias nvim='~/Apps/nvim.appimage'
+    alias vim='~/Apps/nvim.appimage'
     ;;
 
   'Darwin') 
     alias awsw='aws2-wrap'
+    alias vim='nvim'
     ;;
 
   *)
@@ -20,3 +21,4 @@ export FZF_DEFAULT_COMMAND='rg --files --ignore-vcs'
 
 # Aliases
 alias cn="rg '' $HOME/Sync/Notes/cli/ --no-filename |fzf |grep -o '^[^#]*'"
+alias cfg="cd ~/.cfg; vim"
