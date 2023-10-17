458ff5fa9f161336f140490b13dcb2823da92cb7
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2023-10-17 21:35:30 +0300

    tmux terminal toggle script cleanup

--- tmux/scripts/tmux-toggle.bash
+++ tmux/scripts/tmux-toggle.bash
@@ -2,17 +2,14 @@
 
 if [[ $(tmux display-message -p '#{window_panes}') == 1 ]]; then
     tmux split-window -vf -p 30 -c "#{pane_current_path}"
-    # echo "SINGLE PANE"
     exit 0
 fi
 
 tmux list-panes -F '#F' | grep -q Z
 if [ $? -eq 0 ]; then
-    # echo "Command succeeded"
     tmux resize-pane -Z
     tmux select-pane -l
 else
-    # echo "Command failed"
     tmux select-pane -t 0
     tmux resize-pane -Z
 fi
