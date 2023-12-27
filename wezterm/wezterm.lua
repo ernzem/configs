89df7a6c4166b8588b1fd79217dd9f0f8e80e31f
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2023-12-27 11:21:31 +0200

    wezterm: os specific settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,36 +1,51 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
-local config = {}
+-- Maximize on startup
+local mux = wezterm.mux
+wezterm.on('gui-startup', function()
+    local tab, pane, window = mux.spawn_window({})
+    window:gui_window():maximize()
+end)
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
+end
+
+-- This table will hold the configuration.
+local config = {}
+
+local os = wezterm.target_triple
+-- if os == 'x86_64-pc-windows-msvc' then
+-- end
+
+if os == 'x86_64-apple-darwin' or os == 'aarch64-apple-darwin' then
+    config.font_size = 14
+    config.freetype_load_target = "Light"
+end
+
+if os == 'x86_65-unknown-linux-gnu' then
+    config.font_size = 12
+    config.freetype_render_target = "Normal"
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font{
-  family = 'JetBrainsMono Nerd Font',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.font = wezterm.font {
+    family = 'JetBrainsMono Nerd Font',
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 }
-config.font_size = 14
+
 config.hide_tab_bar_if_only_one_tab = true
-config.freetype_load_target = "Light"
--- config.freetype_render_target = "Normal"
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = '0cell',
-  right = '0cell',
-  top = '0cell',
-  bottom = '0cell',
+    left = '0cell',
+    right = '0cell',
+    top = '0cell',
+    bottom = '0cell',
 }
 -- For example, changing the color scheme:
--- config.color_scheme = 'Tomorrow'
--- config.color_scheme = 'Default Light (base16)'
--- config.color_scheme = '3024 (base16)'
-config.color_scheme = 'Mexico Light (base16)'
+-- config.color_scheme = 'Mexico Light (base16)'
+config.color_scheme = 'Kanagawa (Gogh)'
 
--- and finally, return the configuration to wezterm
 return config
