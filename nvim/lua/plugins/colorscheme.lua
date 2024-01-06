0183428370718996d073be5956b5b245925e8592
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-01-06 21:53:11 +0200

    kanarawa config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
--- vim.cmd.colorscheme 'github_light_high_contrast'
--- require('kanagawa').setup({
---     terminalColors = false,
---     colors = {
---         theme = {
---             all = {
---                 ui = {
---                     bg_gutter = "none"
---                 }
---             }
---         }
---     }
--- })
+vim.cmd.colorscheme 'github_light_high_contrast'
+require('kanagawa').setup({
+    terminalColors = false,
+    colors = {
+        theme = {
+            all = {
+                ui = {
+                    bg_gutter = "none"
+                }
+            }
+        }
+    }
+})
 
 vim.opt.background = "dark"
 vim.cmd("colorscheme kanagawa-wave")
