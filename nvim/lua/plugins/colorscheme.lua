d3c54fe98075d1938f337f841cebd321db01e6ee
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-02-08 11:40:04 +0200

    nvim gihtub darken inactive pane

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,26 @@ require('kanagawa').setup({
 })
 
 -- vim.cmd("colorscheme kanagawa-wave")
+require('github-theme').setup({
+    options = {
+        terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+        dim_inactive = true     -- Non focused panes set to alternative background
+    },
+    palettes = {
+        github_light_high_contrast = {
+            bg0 = '#eeeeee', -- Alt backgrounds (floats, statusline, ...)
+        },
+    },
+    specs = {
+        github_light_high_contrast = {
+            inactive = 'bg0'
+        }
+    },
+    groups = {
+        all = {
+            NormalNC = { fg = 'fg1', bg = 'inactive' }, -- Non-current windows
+        },
+    }
+})
 
-vim.opt.background = "light"
 vim.cmd("colorscheme github_light_high_contrast")
