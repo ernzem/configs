927d998e86b5de97f34c723c4749a39fb803f3d5
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-03-23 21:54:15 +0200

    wezterm: changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 -- Maximize on startup
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function()
+wezterm.on("gui-startup", function()
     local _, _, window = mux.spawn_window({})
     window:gui_window():maximize()
 end)
@@ -21,30 +21,31 @@ local os = wezterm.target_triple
 -- if os == 'x86_64-pc-windows-msvc' then
 -- end
 
-if os == 'x86_64-apple-darwin' or os == 'aarch64-apple-darwin' then
+if os == "x86_64-apple-darwin" or os == "aarch64-apple-darwin" then
     config.font_size = 14
+    config.freetype_load_target = "Light"
 end
 
-if os == 'x86_65-unknown-linux-gnu' then
+if os == "x86_65-unknown-linux-gnu" then
     config.font_size = 12
 end
 
-config.font = wezterm.font {
-    family = 'JetBrainsMono Nerd Font Mono',
-    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-}
+config.font = wezterm.font({
+    family = "JetBrainsMono Nerd Font Mono",
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_padding = {
-    left = '0cell',
-    right = '0cell',
-    top = '0cell',
-    bottom = '0cell',
+    left = "0cell",
+    right = "0cell",
+    top = "0cell",
+    bottom = "0cell",
 }
 -- For example, changing the color scheme:
--- config.color_scheme = 'Mexico Light (base16)'
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = "Mexico Light (base16)"
+-- config.color_scheme = 'Kanagawa (Gogh)'
 
 -- Use non gui tab style
 config.use_fancy_tab_bar = false
