2678a06678b1dce5e071b8884c21a63ab8dfbe21
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-04-06 11:00:11 +0300

    nvmi go: use organize imports temporary until confrom fmt is ifxed

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,4 +1,27 @@
 local go_file = "*.go"
+-----------------------------Auto add/remove/organize imports before save------------------------
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = go_file,
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+
+        local timeout = 1000 -- milliseconds
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout)
+
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+        vim.lsp.buf.format({ async = false })
+    end,
+})
+
 ----------------------------CLI Tool Behave Like LSP------------------------------------
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
