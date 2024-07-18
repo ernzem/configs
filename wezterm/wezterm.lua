b9e84ee03263013c988ce33ceeee229ca6249024
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-07-18 10:16:31 +0300

    wezterm theme, font changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,48 +7,51 @@ wezterm.on("gui-startup", function()
     window:gui_window():maximize()
 end)
 
-local c = wezterm.config_builder()
+local config = wezterm.config_builder()
 
 -- if  wezterm.target_triple:find("windows") ~= nil then
 -- end
 
 if wezterm.target_triple:find("darwin") ~= nil then
-    c.font_size = 14
+    config.font_size = 14
     -- config.freetype_load_target = "Light"
 end
 
 if wezterm.target_triple:find("linux") ~= nil then
-    c.font_size = 12
-    c.window_decorations = "RESIZE"
+    config.font_size = 10.8
+    config.window_decorations = "RESIZE"
 end
 
-c.font = wezterm.font({
+config.font = wezterm.font({
     family = "JetBrainsMono Nerd Font Mono",
     harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 
-c.hide_tab_bar_if_only_one_tab = true
--- config.window_decorations = "RESIZE"
-c.window_padding = {
+config.window_padding = {
     left = "0cell",
     right = "0cell",
     top = "0cell",
     bottom = "0cell",
 }
--- For example, changing the color scheme:
+
 -- c.color_scheme = "Mexico Light (base16)"
-c.color_scheme = "Kanagawa (Gogh)"
+-- c.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = 'Default Dark (base16)'
+config.colors = {
+    background = "#232326",
+}
 
 -- Use non gui tab style
-c.use_fancy_tab_bar = false
-c.tab_max_width = 40
-c.switch_to_last_active_tab_when_closing_tab = true
-c.tab_bar_at_bottom = true
-c.scroll_to_bottom_on_input = true
+config.use_fancy_tab_bar = false
+config.tab_max_width = 40
+config.switch_to_last_active_tab_when_closing_tab = true
+config.tab_bar_at_bottom = true
+config.scroll_to_bottom_on_input = true
+
 -- timeout_milliseconds defaults to 1000 and can be omitted
 -- config.disable_default_key_bindings = true
 -- config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
-c.keys = {
+config.keys = {
     {
         key = "a",
         mods = "CTRL",
@@ -70,4 +73,4 @@ c.keys = {
         end),
     },
 }
-return c
+return config
