0a458cf619eaabf8fa3149478b2b3a37d34cbbc5
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-10-04 15:58:54 +0300

    nvim vsode integration fix with nvim autoread note

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,16 @@
-require("defaults")
 require("keymaps")
 
+-- Solves nvim 0.10.x bug with vscode where messages were printed in vscode output pannel
+if vim.g.vscode then
+	vim.opt.cmdheight = 1
+	return
+end
+
 if not vim.g.vscode then
-    require("plugin_manager")
-    require("commands")
-    require("output_buffer")
-    require("statusline")
-    require("winbar")
+	require("defaults")
+	require("plugin_manager")
+	require("commands")
+	require("output_buffer")
+	require("statusline")
+	require("winbar")
 end
--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,7 +1,8 @@
 local augroup = vim.api.nvim_create_augroup
 local utils = require('utils')
 
--- Triger `autoread` when files changes on disk
+-- NOTE: Might not be needed with nvim 0.12
+-- Triger `autoread` when files changes on disk.
 -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
 -- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
 vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
