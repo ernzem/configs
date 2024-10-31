4d45d971e3911f3ecc29a7b78b19bd991fef17e0
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2024-10-31 21:47:34 +0200

    nvim: terminal insert autocommands

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,4 +1,5 @@
 local augroup = vim.api.nvim_create_augroup
+local set = vim.opt_local
 
 -- NOTE: Might not be needed with nvim 0.12
 -- Triger `autoread` when files changes on disk.
@@ -12,15 +13,36 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHo
 -- Trim trailing whitespaces
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = augroup("TrimTrailingWhitespaces", {}),
+	desc = "trim whitespaces",
 	pattern = "*",
 	command = [[%s/\s\+$//e]],
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight-yank", { clear = true }),
 	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	group = augroup("terminal-new-buffer", { clear = true }),
+	desc = "Open terminal in insert mode, turn off line numbers",
+	callback = function()
+		set.number = false
+		set.relativenumber = false
+		set.scrolloff = 0
+		vim.cmd.startinsert()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufEnter" }, {
+	group = augroup("enter-terminal-buffer", { clear = true }),
+    pattern = "term://*",
+	desc = "Go to insert mode when entering terminal buffer",
+	callback = function()
+		vim.cmd.startinsert()
 	end,
 })
 
