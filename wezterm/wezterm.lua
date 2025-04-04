9c3cd08c470c7309be9f30d711fa68403c2cfb63
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2025-04-04 19:51:29 +0300

    wezterm: tweeaks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+local win_frame_state = "RESIZE"
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
@@ -19,7 +20,7 @@ end)
 if wezterm.target_triple:find("darwin") ~= nil then
 	config.font_size = 14
 	config.window_decorations = "RESIZE"
-    config. freetype_load_target = "Light"
+	config.freetype_load_target = "Light"
 end
 
 if wezterm.target_triple:find("linux") ~= nil then
@@ -28,23 +29,23 @@ if wezterm.target_triple:find("linux") ~= nil then
 end
 
 config.font_rules = {
-  {
-    intensity = 'Bold',
-    italic = false,
-    font = wezterm.font("JetBrains Mono", {weight="Bold", stretch="Normal", style="Normal"})
-  },
-  {
-    intensity = 'Bold',
-    italic = true,
-    font = wezterm.font("JetBrains Mono", {weight="Bold", stretch="Normal", style="Italic"})
-  },
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+	},
 }
 config.font = wezterm.font({
 	-- family = "JetBrainsMono Nerd Font Mono",
-	family = "JetBrains Mono Medium",
-	-- family = "JetBrains Mono",
-    -- family = "Fira Code",
+	-- family = "JetBrains Mono Medium",
+	-- family = "Fira Code",
 	-- family = "Hack Nerd Font Mono",
+	family = "JetBrains Mono",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	-- weight = "Medium",
 	-- weight = "Regular",
@@ -55,16 +56,16 @@ config.font = wezterm.font({
 config.max_fps = 144
 config.scroll_to_bottom_on_input = true
 config.window_padding = { left = "0cell", right = "0cell", top = "0cell", bottom = "0cell" }
-config.line_height = 1.25
+config.line_height = 1.2
 
 -- config.color_scheme = "Default Dark (base16)"
 -- config.color_scheme = "Alabaster"
 config.color_scheme = "Mexico Light (base16)"
 config.colors = {
 	-- background = "#232326",
-	background = "#F2F2F2",
+	background = "#F6F6F6",
 	-- background = "#E6E6E6",
-    foreground = "#000000"
+	foreground = "#000000",
 }
 
 config.tab_max_width = 40
@@ -95,6 +96,20 @@ config.keys = {
 				tab:set_zoomed(false)
 				panes[2].pane:activate()
 			end
+		end),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(_, pane)
+         local overrides = window:get_config_overrides()
+         if overrides.window_decorations == 'RESIZE' then
+            overrides.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+         else
+            overrides.window_decorations = 'RESIZE'
+         end
+
+         -- wezterm.reload_configuration()
 		end),
 	},
 }
