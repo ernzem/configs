eb22be5ef19d06896470104b69935560c092a41a
Ernestas Žemaitis <zemaitis.ernestas@gmail.com>
2025-06-23 21:45:36 +0300

    separate zsh shell config

--- scripts/shell_cfg_install.sh
+++ scripts/shell_cfg_install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # TODO: detect default shell and check its source files there
-
-FILENAME=~/.zshrc
+FILENAME=~/.bashrc
+TARGET_ZSH="source ~/.cfg/shell/my_zsh"
 TARGET_STR="source ~/.cfg/shell/my_configs"
+
+# If zsh_version it means zsh is defualt shell
+if [ ! -z ${ZSH_VERSION+x} ]; then
+    FILENAME=~/.zshrc
+fi
 
 case `grep -Fx "$TARGET_STR" "$FILENAME"  >/dev/null; echo $?` in
   0)
@@ -12,8 +17,13 @@ case `grep -Fx "$TARGET_STR" "$FILENAME"  >/dev/null; echo $?` in
   1)
     echo "# Import my configurations" >> $FILENAME
     echo "${TARGET_STR}" >> $FILENAME
+
+    # Push zsh config if zsh is default
+    if [ ! -z ${ZSH_VERSION+x} ]; then
+        echo "${TARGET_ZSH}" >> $FILENAME
+    fi
     ;;
   *)
-    echo "ERROR!"
+    echo "Unexpected response.ERROR!"
     ;;
 esac
