18c5ef1fb240b33ce4d6ac2de3971bdb4fba40f3
ernestas <ernestas@xps.lan>
2026-01-01 17:43:57 +0200

    github latest release: apt, dnf install curl

--- scripts/github-get-latest-release.sh
+++ scripts/github-get-latest-release.sh
@@ -2,6 +2,23 @@
 
 # curl -s https://api.github.com/repos/wezterm/wezterm/releases/latest | grep '"browser_download_url":' | grep '.fedora39.x86_64.rpm"' |  grep -o 'https://[^"]*' | wget -i -
 
+# Check if curl is installed:
+if ! command -v curl >/dev/null 2>&1; then
+
+    if command -v apt > /dev/null 2>&1; then
+        sudo apt install -y curl
+
+    elif command -v dnf > /dev/null 2>&1; then
+        sudo dnf install -y curl
+
+    else
+        echo "No known package manager found"
+        exit 1
+    fi
+
+fi
+
+
 # Params: 1. owner/repos 2. regex of file to download
 latest_release_url() {
     curl -s https://api.github.com/repos/$1/releases/latest | grep '"browser_download_url":' | grep $2 |  grep -o 'https://[^"]*'
